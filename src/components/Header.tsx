cac11b85b8b505b9a77eab972402f87b0128c6ac
Walefe <walefedev@gmail.com>
2021-03-16 10:51:21 -0300

    feat: create Header component.

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+interface HeaderProps {
+  genreTitle: string;
+}
+
+export function Header({genreTitle}: HeaderProps) {
+  return (
+    <header>
+        <span className="category">Categoria:<span> {genreTitle}</span></span>
+     </header>
+  )
+}
